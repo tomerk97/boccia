084aaff0012601c93e95a75f050badcabb4d3251
tomerk97 <tomerk1997@gmail.com>
2020-09-26 18:07:47 +0300

    Updates

---
+++ precache-manifest.6596d8d35b8bebc0496a985964080b43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3732eb4dfd5d1a5427be0577b5dc27e",
+    "url": "/boccia/index.html"
+  },
+  {
+    "revision": "99bbb6e1fc429f8d2c0a",
+    "url": "/boccia/static/css/main.c93f1814.chunk.css"
+  },
+  {
+    "revision": "c9f462b68a6ff8477ca5",
+    "url": "/boccia/static/js/2.4e97f2d2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/boccia/static/js/2.4e97f2d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99bbb6e1fc429f8d2c0a",
+    "url": "/boccia/static/js/main.899c30d8.chunk.js"
+  },
+  {
+    "revision": "238376d4608073255e1d",
+    "url": "/boccia/static/js/runtime-main.74d11a01.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/boccia/static/media/logo.5d5d9eef.svg"
+  }
+]);
